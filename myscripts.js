be5af033c9059483722b43e3cde8068763ac602d
khameer_13 <khameer2002@gmail.com>
2023-08-24 18:41:13 +0530

    skeletonUpdated

--- myscripts.js
+++ myscripts.js
@@ -2,24 +2,27 @@ const slides = document.querySelectorAll(".slide")
 var counter = 0;
 // console.log(slides)
 slides.forEach(
-  (slide,index) => {
+  (slide, index) => {
     slide.style.left = `${index * 100}%`  //to slide left
   }           //we  can alse use top to scroll img vertically
 )        //for that we also need to change tranlate x to y
 
 const goPrev = () => {
   counter--
-  // if(counter>0)
-  // {
+  if (counter < 0) {
+    counter = slides.length-1;
+  }
   slideImage()
 }
 // }
 const goNext = () => {
   counter++
-  // if(counter<4){
- 
-  slideImage()
+  if (counter > slides.length-1) {
+    counter = 0;
   }
+
+  slideImage()
+}
 // }
 
 const slideImage = () => {
